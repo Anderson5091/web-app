f7e4a93890d80dc0838c82221207d5c589a30b83
Anderson5091 <anderson5091@gmail.com>
2026-06-26 12:01:51 -0400

    fix env variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,3 @@
 export const ENV = {
-  API_URL: import.meta.env.VITE_API_URL || "https://backend-app-production-0a8a.up.railway.app/api/v1",
+  API_URL: import.meta.env.VITE_API_URL || "https://corequicksend.up.railway.app/api/v1",
 };
